f9cb2446febe3ed6093294fa1f5c897613dd969f
Enrico Lorenzo Rodriguez <elorenzo.rodz@gmail.com>
2025-09-30 10:01:27 +0800

    add: payments schema

---
+++ sql/schema/005_payments.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+
+CREATE TABLE payments (
+    id UUID PRIMARY KEY,
+    amount NUMERIC(10, 2) NOT NULL DEFAULT 0.00,
+	currency TEXT NOT NULL,
+    status TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+
+DROP TABLE payments;
