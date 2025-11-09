3be974446de84c4dbb342301d171be9cd027ec9f
Enrico Lorenzo Rodriguez <elorenzo.rodz@gmail.com>
2025-11-09 14:03:44 +0800

    remove: commons package and added missing env key for refund

--- config/config.go
+++ config/config.go
@@ -6,18 +6,19 @@ import (
 )
 
 type AppConfig struct {
-	APIVersion           string
-	Port                 string
-	DBURL                string
-	GinMode              string
-	MailgunAPIKey        string
-	MailgunSendingDomain string
-	SenderName           string
-	SenderEmail          string
-	StripeSecretKey      string
-	StripeSigningSecret  string
-	TeamName string
-	TeamEmail string
+	APIVersion                string
+	Port                      string
+	DBURL                     string
+	GinMode                   string
+	MailgunAPIKey             string
+	MailgunSendingDomain      string
+	SenderName                string
+	SenderEmail               string
+	StripeSecretKey           string
+	StripeSigningSecret       string
+	StripeRefundSigningSecret string
+	TeamName                  string
+	TeamEmail                 string
 }
 
 func getEnvironmentVariable(key string) (string, error) {
@@ -35,18 +36,48 @@ func LoadEnvironmentVariables() (AppConfig, error) {
 
 	var err error
 
-	if appConfig.APIVersion, err = getEnvironmentVariable("API_VERSION"); err != nil { return appConfig, err }
-	if appConfig.Port, err = getEnvironmentVariable("PORT"); err != nil { return appConfig, err }
-	if appConfig.DBURL, err = getEnvironmentVariable("DB_URL"); err != nil { return appConfig, err }
-	if appConfig.GinMode, err = getEnvironmentVariable("GIN_MODE"); err != nil { return appConfig, err }
-	if appConfig.MailgunAPIKey, err = getEnvironmentVariable("MAILGUN_API_KEY"); err != nil { return appConfig, err }
-	if appConfig.MailgunSendingDomain, err = getEnvironmentVariable("MAILGUN_SENDING_DOMAIN"); err != nil { return appConfig, err }
-	if appConfig.SenderName, err = getEnvironmentVariable("SENDER_NAME"); err != nil { return appConfig, err }
-	if appConfig.SenderEmail, err = getEnvironmentVariable("SENDER_EMAIL"); err != nil { return appConfig, err }
-	if appConfig.StripeSecretKey, err = getEnvironmentVariable("STRIPE_SECRET_KEY"); err != nil { return appConfig, err }
-	if appConfig.StripeSigningSecret, err = getEnvironmentVariable("STRIPE_SIGNING_SECRET"); err != nil { return appConfig, err }
-	if appConfig.TeamName, err = getEnvironmentVariable("TEAM_NAME"); err != nil { return appConfig, err }
-	if appConfig.TeamEmail, err = getEnvironmentVariable("TEAM_EMAIL"); err != nil { return appConfig, err }
+	if appConfig.APIVersion, err = getEnvironmentVariable("API_VERSION"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.Port, err = getEnvironmentVariable("PORT"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.DBURL, err = getEnvironmentVariable("DB_URL"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.GinMode, err = getEnvironmentVariable("GIN_MODE"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.MailgunAPIKey, err = getEnvironmentVariable("MAILGUN_API_KEY"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.MailgunSendingDomain, err = getEnvironmentVariable("MAILGUN_SENDING_DOMAIN"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.SenderName, err = getEnvironmentVariable("SENDER_NAME"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.SenderEmail, err = getEnvironmentVariable("SENDER_EMAIL"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.StripeSecretKey, err = getEnvironmentVariable("STRIPE_SECRET_KEY"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.StripeSigningSecret, err = getEnvironmentVariable("STRIPE_SIGNING_SECRET"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.StripeRefundSigningSecret, err = getEnvironmentVariable("STRIPE_WEBHOOK_SECRET"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.StripeRefundSigningSecret, err = getEnvironmentVariable("STRIPE_REFUND_SIGNING_SECRET"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.TeamName, err = getEnvironmentVariable("TEAM_NAME"); err != nil {
+		return appConfig, err
+	}
+	if appConfig.TeamEmail, err = getEnvironmentVariable("TEAM_EMAIL"); err != nil {
+		return appConfig, err
+	}
 
 	return appConfig, nil
 }
